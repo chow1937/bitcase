073ecc6f5513087733aefb410425c6a90c5a50d4
tonychow <chow1937@gmail.com>
2014-04-14 23:21:04 +0800

    add hashtable API free bucket declaration

--- src/hashtable.h
+++ src/hashtable.h
@@ -37,6 +37,7 @@ int ht_alloc(hash_table *ht, unsigned long size);
 int ht_add(hash_table *ht, void *key, void *value);
 int ht_delete(hash_table *ht, void *key);
 int ht_update(hash_table *ht, void *key, void *value);
+int ht_free_bucket(bucket *bc);
 uint32_t gen_hash(const void *key);
 
 #endif
