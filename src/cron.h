20df6c6ef3ae2956deecece678cde5fac12632a4
tonychow <chow1937@gmail.com>
2014-04-25 17:34:09 +0800

    add some timer intervels macro define

--- src/cron.h
+++ src/cron.h
@@ -1,6 +1,11 @@
 #ifndef _CRON_H
 #define _CRON_H
 
+/*Cron jobs intervals*/
+#define RESIZE_INTERVAL 500
+#define REHASH_INTERVAL 200
+#define MEM_COUNT_INTERVAL 2000
+
 /*Cron jobs define as libuv timer callback*/
 void cron_check_resize(uv_timer_t* handle, int status);
 void cron_rehash(uv_timer_t* handle, int status);
